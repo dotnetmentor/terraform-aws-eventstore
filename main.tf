6d9921799076bd06deeccfb762e981e2ededd182
Kristoffer Ahl <git@77dynamite.com>
2018-09-25 12:27:39 +0200

    - Initial take on a clustered eventstore module.

---
+++ main.tf
@@ -0,0 +1,261 @@
+provider "aws" {
+  version = "~> 1.10.0"
+  region  = "${var.region}"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# KEY PAIR USED FOR EVENTSTORE INSTANCES
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "${var.key_pair_name}"
+  public_key = "${var.key_pair_publickey}"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ASG
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_autoscaling_group" "eventstore" {
+  name                 = "${var.cluster_name}-${var.environment}-eventstore-asg"
+  launch_configuration = "${aws_launch_configuration.eventstore.id}"
+
+  min_size         = "0"
+  max_size         = "5"
+  desired_capacity = "${var.cluster_size}"
+
+  availability_zones  = "${var.cluster_azs}"
+  vpc_zone_identifier = "${var.cluster_subnets}"
+
+  termination_policies = [
+    "Default",
+  ]
+
+  enabled_metrics = [
+    "GroupMinSize",
+    "GroupMaxSize",
+    "GroupDesiredCapacity",
+    "GroupInServiceInstances",
+    "GroupPendingInstances",
+    "GroupStandbyInstances",
+    "GroupTerminatingInstances",
+    "GroupTotalInstances",
+  ]
+
+  tags = [
+    {
+      key                 = "Name"
+      value               = "${var.cluster_name}-${var.environment}-eventstore"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Environment"
+      value               = "${var.environment}"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Terraform"
+      value               = "true"
+      propagate_at_launch = true
+    },
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "image-type"
+    values = ["machine"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+}
+
+locals {
+  ebs_optimized = {
+    "t2.large" = true
+  }
+}
+
+resource "aws_launch_configuration" "eventstore" {
+  name          = "${var.cluster_name}-${var.environment}-eventstore-lc-${substr(uuid(),0,8)}"
+  image_id      = "${var.instance_ami != "" ? var.instance_ami : data.aws_ami.ubuntu.id}"
+  instance_type = "${var.instance_type}"
+
+  security_groups = ["${aws_security_group.eventstore.id}"]
+  key_name        = "${var.key_pair_name}"
+
+  user_data = "${data.template_file.eventstore_init.rendered}"
+
+  ebs_optimized = "${lookup(local.ebs_optimized, var.instance_type, false)}"
+
+  ebs_block_device {
+    device_name           = "${var.instance_volume_name_prefix}b"
+    volume_size           = "${var.instance_volume_size}"
+    volume_type           = "gp2"
+    encrypted             = "${var.instance_volume_encryption}"
+    delete_on_termination = "${var.instance_volume_protection ? false : true}"
+  }
+
+  ebs_block_device {
+    device_name           = "${var.instance_volume_name_prefix}c"
+    volume_size           = "${var.instance_volume_size}"
+    volume_type           = "gp2"
+    encrypted             = "${var.instance_volume_encryption}"
+    delete_on_termination = "${var.instance_volume_protection ? false : true}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "template_file" "eventstore_init" {
+  template = "${file("${path.module}/user_data.tpl")}"
+
+  vars {
+    environment                           = "${var.environment}"
+    cluster_version                       = "${var.cluster_version}"
+    cluster_size                          = "${var.cluster_size}"
+    cluster_dns                           = "${var.cluster_dns}"
+    external_ip_type                      = "${var.cluster_external_ip_type == "private" ? "local" : "public"}"
+    internal_ip_type                      = "${var.cluster_internal_ip_type == "private" ? "local" : "public"}"
+    log_forwarding_elasticsearch_enabled  = "${var.log_forwarding_elasticsearch_enabled ? true : false}"
+    log_forwarding_elasticsearch_endpoint = "${var.log_forwarding_elasticsearch_endpoint}"
+    log_forwarding_elasticsearch_port     = "${var.log_forwarding_elasticsearch_port}"
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# SECURITY GROUPS
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_security_group" "eventstore" {
+  name   = "${var.cluster_name}-${var.environment}-eventstore-sg"
+  vpc_id = "${var.cluster_vpc_id}"
+
+  tags = {
+    Name        = "${var.cluster_name}-${var.environment}-eventstore-sg"
+    Environment = "${var.environment}"
+    Terraform   = "true"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "ssh_external" {
+  type = "ingress"
+
+  description     = "SSH (External)"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = "${var.cluster_allowed_cidr_blocks}"
+
+  security_group_id = "${aws_security_group.eventstore.id}"
+}
+
+resource "aws_security_group_rule" "client_tcp_external" {
+  type = "ingress"
+
+  description     = "Clients TCP (External)"
+  from_port   = 1113
+  to_port     = 1113
+  protocol    = "tcp"
+  cidr_blocks = "${var.cluster_allowed_cidr_blocks}"
+
+  security_group_id = "${aws_security_group.eventstore.id}"
+}
+
+resource "aws_security_group_rule" "client_http_external" {
+  type = "ingress"
+
+  description     = "Clients HTTP (External)"
+  from_port   = 2113
+  to_port     = 2113
+  protocol    = "tcp"
+  cidr_blocks = "${var.cluster_allowed_cidr_blocks}"
+
+  security_group_id = "${aws_security_group.eventstore.id}"
+}
+
+resource "aws_security_group_rule" "tcp_internal_1112" {
+  type = "ingress"
+
+  description     = "TCP (Internal)"
+  from_port = 1112
+  to_port   = 1112
+  protocol  = "tcp"
+  self      = true
+
+  security_group_id = "${aws_security_group.eventstore.id}"
+}
+
+resource "aws_security_group_rule" "tcp_internal_1113" {
+  type = "ingress"
+
+  description     = "TCP (Internal)"
+  from_port = 1113
+  to_port   = 1113
+  protocol  = "tcp"
+  self      = true
+
+  security_group_id = "${aws_security_group.eventstore.id}"
+}
+
+resource "aws_security_group_rule" "tcp_internal_2112" {
+  type = "ingress"
+
+  description     = "TCP (Internal)"
+  from_port = 2112
+  to_port   = 2112
+  protocol  = "tcp"
+  self      = true
+
+  security_group_id = "${aws_security_group.eventstore.id}"
+}
+
+resource "aws_security_group_rule" "tcp_internal_2113" {
+  type = "ingress"
+
+  description     = "TCP (Internal)"
+  from_port = 2113
+  to_port   = 2113
+  protocol  = "tcp"
+  self      = true
+
+  security_group_id = "${aws_security_group.eventstore.id}"
+}
+
+resource "aws_security_group_rule" "allow_all_outgoing" {
+  type = "egress"
+
+  description     = "Allow all outgoing"
+  from_port   = 0
+  to_port     = 0
+  protocol    = -1
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = "${aws_security_group.eventstore.id}"
+}
---
+++ user_data.tpl
@@ -0,0 +1,130 @@
+#!/bin/bash
+set -e
+
+export DATA_MOUNT_PATHS=('/dev/xvdb' '/dev/xvdc')
+export DATA_POOL_NAME='ebs'
+export DATA_DIR="/$${DATA_POOL_NAME:?}/data"
+export DEBIAN_FRONTEND='noninteractive'
+export ES_VERSION='${cluster_version}'
+export ES_INSTALL_URL="https://packagecloud.io/install/repositories/EventStore/EventStore-OSS/script.deb.sh"
+export ES_DB_PATH="$${DATA_DIR:?}/eventstore/data"
+export ES_LOG_PATH="$${DATA_DIR:?}/eventstore/log"
+export LOG_FORWARDING_ENABLED='${log_forwarding_elasticsearch_enabled}'
+
+echo
+echo 'Ensuring apt-get is updated (at least once)'
+apt-get update
+
+echo
+echo 'Adding hostname to hosts'
+echo "127.0.0.1 $(hostname)" >> /etc/hosts
+cat /etc/hosts
+
+echo
+echo 'Ensure journald is setup'
+mkdir -p /var/log/journal
+systemd-tmpfiles --create --prefix /var/log/journal
+systemctl restart systemd-journald
+
+if [[ "$${LOG_FORWARDING_ENABLED}" == "true" ]]; then
+  echo
+  echo 'Installing fluentbit'
+  wget -qO - https://packages.fluentbit.io/fluentbit.key | apt-key add -
+  echo "
+  ## fluentbit
+  deb https://packages.fluentbit.io/ubuntu/xenial xenial main" >> /etc/apt/sources.list
+  apt-get update
+  apt-get install td-agent-bit
+
+  echo
+  echo 'Configure fluentbit'
+  echo "[SERVICE]
+    Flush         1
+    Log_Level     info
+    Daemon        Off
+    HTTP_Server   Off
+    Parsers_File parsers.conf
+    Plugins_File plugins.conf
+
+[INPUT]
+    Name            systemd
+    Path            /var/log/journal/
+    Read_From_Tail  True
+    Tag             eventstore-host-${environment}
+    Mem_Buf_Limit   20MB
+
+[OUTPUT]
+    Name            es
+    Match           *
+    Host            ${log_forwarding_elasticsearch_endpoint}
+    Port            ${log_forwarding_elasticsearch_port}
+    tls             On
+    tls.verify      Off
+    Logstash_Format On
+    Logstash_Prefix logstash-${environment}
+    Time_Key        time
+    Retry_Limit     False
+    Include_Tag_Key True
+    Tag_Key         FLB_KEY
+" > /etc/td-agent-bit/td-agent-bit.conf
+
+  echo
+  echo 'Starting fluentbit'
+  service td-agent-bit restart
+  service td-agent-bit status
+fi
+
+echo
+echo 'Installing Eventstore'
+apt-get install tzdata curl iproute2 -y
+curl -s "$${ES_INSTALL_URL}" | bash
+apt-get install eventstore-oss="$${ES_VERSION}" -y
+
+echo
+echo 'Installing ZFS'
+apt-get install zfsutils-linux -y
+
+echo
+echo 'Preparing mirrored ZFS pool'
+zpool create -f "$${DATA_POOL_NAME:?}" mirror "$${DATA_MOUNT_PATHS[0]}" "$${DATA_MOUNT_PATHS[1]}"
+sleep 3
+zpool status
+mkdir -p "$${DATA_DIR:?}"
+
+echo
+echo 'Preparing eventstore directories'
+mkdir -p $${ES_DB_PATH}
+mkdir -p $${ES_LOG_PATH}
+sudo chown -R eventstore:eventstore $${ES_DB_PATH}
+sudo chown -R eventstore:eventstore $${ES_LOG_PATH}
+
+echo
+echo 'Performing cleanup'
+apt-get autoremove
+apt-get clean
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* || true
+
+echo
+echo 'Configuring eventstore'
+cat <<EOFCONF > "/etc/eventstore/eventstore.conf"
+---
+RunProjections: None
+StartStandardProjections: True
+Db: $${ES_DB_PATH}
+Log: $${ES_LOG_PATH}
+ClusterSize: ${cluster_size}
+ClusterGossipPort: 2112
+ClusterDns: ${cluster_dns}
+ExtIp: $$(ec2metadata --local-ipv4)
+IntIp: $$(ec2metadata --local-ipv4)
+ExtIpAdvertiseAs: $$(ec2metadata --${external_ip_type}-ipv4)
+IntIpAdvertiseAs: $$(ec2metadata --${internal_ip_type}-ipv4)
+IntHttpPrefixes: http://*:2112/
+ExtHttpPrefixes: http://*:2113/
+AddInterfacePrefixes: false
+EOFCONF
+
+echo
+echo 'Starting eventstore'
+systemctl start eventstore
+systemctl status eventstore
